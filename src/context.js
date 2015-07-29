3e2d2e6631bb6be53bb9f16b3d13dd25fc5439f2
Gevorg Harutyunyan <gevharut@gmail.com>
2015-07-29 06:43:35 +0400

    Header Notifications Dropdown

--- src/context.js
+++ src/context.js
@@ -1,4 +1,13 @@
 module.exports = {
 	title: "Modularity - Free Admin Dashboard Theme | HTML Version",
-	page: ""
+	page: "",
+	img: {
+		faces: [
+			'assets/faces/3.jpg',
+			'assets/faces/5.jpg',
+			'assets/faces/8.jpg',
+			'assets/faces/6.jpg',
+			'assets/faces/7.jpg',
+		]
+	}
 };
