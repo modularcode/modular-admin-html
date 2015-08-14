45e3c1a1c362d84f5c3c58e303d85e770e55eaac
Aram Manukyan <aram.manukyan92@gmail.com>
2015-08-14 17:38:02 +0400

    Sidebar collapse

--- src/app/_common/sidebar/sidebar.js
+++ src/app/_common/sidebar/sidebar.js
@@ -3,5 +3,12 @@ $(function () {
 	$('#sidebar-menu').metisMenu({
 		activeClass: 'open'
 	});
+
+
+	$('.collapse-btn').on('click', function(event){
+		event.preventDefault();
+		
+		$(".main-app").toggleClass("sidebar-collapsed sidebar-opened");
+	});
 	
 });
