33b5569dd95b7045bfd4ab08d53139d2cee2b9d2
Aram Manukyan <aram.manukyan92@gmail.com>
2015-09-06 12:52:24 +0400

    Forgot password page

---
+++ src/auth/reset/reset.js
@@ -0,0 +1,25 @@
+//LoginForm validation
+$(function() {
+	if (!$('#reset-form').length) {
+        return false;
+    }
+
+    var resetValidationSettings = {
+	    rules: {
+	        email1: {
+	            required: true,
+	            email: true
+	        }
+	    },
+	    messages: {
+	        email1: {
+	            required: "Please enter email address",
+	            email: "Please enter a valid email address"
+	        }
+	    }
+	}
+
+	$.extend(resetValidationSettings, validationDefaultSettings);
+
+    $('#reset-form').validate(resetValidationSettings);
+})
