1b74ac08a87a624de5dab6c48467e29600b8552c
Gevorg Harutyunyan <gevharut@gmail.com>
2015-11-01 05:30:06 +0400

    themes tasks init

---
+++ build/tasks/app-themes.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var config = require('../config');
+
+module.exports.task = function(gulp, plugins, paths) {
+
+	// ToDo
+
+	// Iterate over all -theme files
+	// compile each theme into css
+
+};
