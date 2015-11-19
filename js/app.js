e14e30f088ce740a9f7ba90ede45e163950eafbe
Gevorg Harutyunyan <gevharut@gmail.com>
2015-11-20 01:45:06 +0400

    Update 2015-11-19T21:44:59.599Z

--- js/app.js
+++ js/app.js
@@ -612,7 +612,35 @@ $(function() {
         return false;
     }
 
-    var data = [
+    // drawing visits chart
+    drawVisitsChart();
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+       
+       var el = e.target;
+       var item = $(el).attr('href').replace('#', '');
+
+       var chartSelector = "#dashboard-" + item + "-chart";
+
+       if ($(chartSelector).has('svg').length) {
+            $(chartSelector).empty();
+       }
+
+       switch(item){
+            case 'visits':
+                drawVisitsChart();
+                break;
+             case 'downloads':
+                drawDownloadsChart();
+                break;
+       }
+    })
+
+});
+
+
+function drawVisitsChart(){
+    var dataVisits = [
         { x: '2015-09-01', y: 70},
         { x: '2015-09-02', y: 75 },
         { x: '2015-09-03', y: 50},
@@ -625,7 +653,7 @@ $(function() {
 
     Morris.Line({
         element: 'dashboard-visits-chart',
-        data: data,
+        data: dataVisits,
         xkey: 'x',
         ykeys: ['y'],
         ymin: 'auto 40',
@@ -647,8 +675,68 @@ $(function() {
             tinycolor(config.colorPrimary.toString()).darken(10).toString()
         ],
     });
+}
 
-});
+function drawDownloadsChart(){
+
+    var dataDownloads = [
+        { 
+            year: '2006',
+            downloads: 1300
+        },
+        { 
+            year: '2007', 
+            downloads: 1526
+        },
+        { 
+            year: '2008', 
+            downloads: 2000
+        },
+        { 
+            year: '2009', 
+            downloads: 1800
+        },
+        { 
+            year: '2010', 
+            downloads: 1650
+        },    
+        { 
+            year: '2011', 
+            downloads: 620
+        },
+        { 
+            year: '2012', 
+            downloads: 1000
+        },
+        { 
+            year: '2013', 
+            downloads: 1896
+        },
+        { 
+            year: '2014', 
+            downloads: 850
+        },
+        { 
+            year: '2015', 
+            downloads: 1500
+        }  
+    ];
+
+
+    Morris.Bar({
+        element: 'dashboard-downloads-chart',
+        data: dataDownloads,
+        xkey: 'year',
+        ykeys: ['downloads'],
+        labels: ['Downloads'],
+        hideHover: 'auto',
+        resize: true,
+        barColors: [
+            config.colorPrimary.toString(),
+            tinycolor(config.colorPrimary.toString()).darken(10).toString()
+        ],
+    });
+}
 //LoginForm validation
 $(function() {
 	if (!$('.form-control').length) {
@@ -730,7 +818,7 @@ $(function() {
 });
 $(function () {
 
-	$('#sidebar-menu').metisMenu({
+	$('#sidebar-menu, #customize').metisMenu({
 		activeClass: 'open'
 	});
 
@@ -756,6 +844,29 @@ $(function() {
 		});
 	});
 })
+var modalMedia = {
+	$el: $("#modal-media"),
+	result: {},
+	options: {},
+	open: function(options) {
+		options = options || {};
+		this.options = options;
+
+
+		this.$el.modal('show');
+	},
+	close: function() {
+		if ($.isFunction(this.options.beforeClose)) {
+			this.options.beforeClose(this.result);
+		}
+
+		this.$el.modal('hide');
+
+		if ($.isFunction(this.options.afterClose)) {
+			this.options.beforeClose(this.result);
+		}
+	}
+};
 $(function() {
 
 	$("body").addClass("loaded");
