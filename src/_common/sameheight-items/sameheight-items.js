c1e788739a16f9f7cd01397459d7f58b5e5e4b3a
Gevorg Harutyunyan <gevharut@gmail.com>
2015-12-10 14:38:43 +0400

    Items improvements

---
+++ src/_common/sameheight-items/sameheight-items.js
@@ -0,0 +1,42 @@
+$(function() {
+	setSameHeights();
+
+	var resizeTimer;
+
+	$(window).resize(function() {
+		clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(setSameHeights, 150);
+	});
+});
+
+
+function setSameHeights() {
+
+	var viewport = ResponsiveBootstrapToolkit.current();
+
+	$('.sameheight-container').each(function() {
+
+		var $items = $(this).find(".sameheight-item");
+
+		// Get max height of items in container
+		var maxHeight = 0;
+
+		$items.each(function() {
+			$(this).css({height: 'auto'});
+			maxHeight = Math.max(maxHeight, $(this).innerHeight());
+		});
+
+
+		// Set heights of items
+		$items.each(function() {
+			// Ignored viewports for item
+			var excludedStr = $(this).data('exclude') || '';
+			var excluded = excludedStr.split(',');
+
+			// Set height of element if it's not excluded on 
+			if (excluded.indexOf(viewport) === -1) {
+				$(this).innerHeight(maxHeight);
+			}
+		});
+	});
+}
--- src/main.js
+++ src/main.js
@@ -2,53 +2,8 @@ $(function() {
 
 	$("body").addClass("loaded");
 
-	/***********************************************
-	*            Same height columns
-	***********************************************/
-
-	setSameHeights();
-
-	var resizeTimer;
-
-	$(window).resize(function() {
-		clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(setSameHeights, 150);
-	});
-
 });
 
-var numCalls = 0;
-
-function setSameHeights() {
-
-	var viewport = ResponsiveBootstrapToolkit.current();
-
-	$('.sameheight-container').each(function() {
-
-		var $items = $(this).find(".sameheight-item");
-
-		// Get max height of items in container
-		var maxHeight = 0;
-
-		$items.each(function() {
-			$(this).css({height: 'auto'});
-			maxHeight = Math.max(maxHeight, $(this).innerHeight());
-		});
-
-
-		// Set heights of items
-		$items.each(function() {
-			// Ignored viewports for item
-			var excludedStr = $(this).data('exclude') || '';
-			var excluded = excludedStr.split(',');
-
-			// Set height of element if it's not excluded on 
-			if (excluded.indexOf(viewport) === -1) {
-				$(this).innerHeight(maxHeight);
-			}
-		});
-	});
-}
 
 
 /***********************************************
