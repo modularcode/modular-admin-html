4431b22b5c3e06ec1d67607f2212ca83914d897b
Gevorg Harutyunyan <gevharut@gmail.com>
2015-12-17 19:08:11 +0400

    Remove customize script

--- src/app/_common/sidebar/customize/customize.js
+++ src/app/_common/sidebar/customize/customize.js
@@ -1,65 +1,65 @@
 $(function () {
 
-	// set theme type
-	var theme = localStorage.getItem('theme') || null;
+	// // set theme type
+	// var theme = localStorage.getItem('theme') || null;
 
-	// remove css link
-	removeCssLink(theme);
+	// // remove css link
+	// // removeCssLink(theme);
 
-	// load css link
-	loadCssLink(theme);
+	// // load css link
+	// loadCssLink(theme);
 	
 
-	$('.color-item').each(function() {
-		if (theme === $(this).data('theme')) {
-			$(this).addClass("active");
-		}
-	});
+	// $('.color-item').each(function() {
+	// 	if (theme === $(this).data('theme')) {
+	// 		$(this).addClass("active");
+	// 	}
+	// });
 
 
-	$('.color-item').on('click', function() {
-		$('.color-item').removeClass('active');
-		$(this).addClass('active');
+	// $('.color-item').on('click', function() {
+	// 	$('.color-item').removeClass('active');
+	// 	$(this).addClass('active');
 		
-		// set previous version of theme type
-		var prevTheme = theme;
+	// 	// set previous version of theme type
+	// 	var prevTheme = theme;
 
-		// set theme type
-		theme = $(this).data('theme');
-		localStorage.setItem('theme', theme);
+	// 	// set theme type
+	// 	theme = $(this).data('theme');
+	// 	localStorage.setItem('theme', theme);
 
-		// load css link
-		//loadCssLink(theme);
+	// 	// load css link
+	// 	//loadCssLink(theme);
 
-		// remove css link
-		//removeCssLink(prevTheme);
-	});
+	// 	// remove css link
+	// 	//removeCssLink(prevTheme);
+	// });
 
 
-	function loadCssLink(theme){
-		var themeName = "app";
+	// function loadCssLink(theme){
+	// 	var themeName = "app";
 
-		if (theme) {
-			themeName += "-" + theme;
-		}
+	// 	if (theme) {
+	// 		themeName += "-" + theme;
+	// 	}
 
-		$.getCSS("/css/" + themeName + ".css");
-	}
+	// 	$.getCSS("/css/" + themeName + ".css");
+	// }
 
-	function removeCssLink(theme){
-		var themeName = "app";
+	// function removeCssLink(theme){
+	// 	var themeName = "app";
 
-		if (theme) {
-			themeName += "-" + theme;
-		}
+	// 	if (theme) {
+	// 		themeName += "-" + theme;
+	// 	}
 
-		themeName += ".css";
+	// 	themeName += ".css";
 
-		$("head").find("link[rel=stylesheet]").attr("href", function (i, src) {
-			if (src.search(themeName) >= 0) {
-				$(this).remove();
-			}
-		});
-	}
+	// 	$("head").find("link[rel=stylesheet]").attr("href", function (i, src) {
+	// 		if (src.search(themeName) >= 0) {
+	// 			$(this).remove();
+	// 		}
+	// 	});
+	// }
 	
 });
