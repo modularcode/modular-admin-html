7f55889c4ded0e405f6e2dd9e131ad98a595f252
Gevorg Harutyunyan <gevharut@gmail.com>
2015-12-24 04:33:35 +0400

    dashboard sales breakdown chart adjustment

--- src/app/dashboard/sales-breakdown/sales-breakdown.js
+++ src/app/dashboard/sales-breakdown/sales-breakdown.js
@@ -1,19 +1,25 @@
 $(function() {
 
-    if (!$('#sales-chart').length) {
+    var $dashboardSalesBreakdownChart = $('#dashboard-sales-breakdown-chart');
+
+    if (!$dashboardSalesBreakdownChart.length) {
         return false;
     }
 
     Morris.Donut({
-        element: 'sales-chart',
+        element: 'dashboard-sales-breakdown-chart',
         data: [{ label: "Download Sales", value: 12 },
             { label: "In-Store Sales", value: 30 },
             { label: "Mail-Order Sales", value: 20 } ],
         resize: true,
         colors: [
             tinycolor(config.colorPrimary.toString()).lighten(10).toString(),
-            tinycolor(config.colorPrimary.toString()).darken(10).toString(),
+            tinycolor(config.colorPrimary.toString()).darken(8).toString(),
             config.colorPrimary.toString()
         ],
     });
+
+    var $sameheightContainer = $dashboardSalesBreakdownChart.closest(".sameheight-container");
+
+    setSameHeights($sameheightContainer);
 })
