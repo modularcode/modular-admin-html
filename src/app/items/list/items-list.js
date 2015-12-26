e2f5216f62e703349789a673c738b15cea1b7cde
Gevorg Harutyunyan <gevharut@gmail.com>
2015-12-27 00:49:57 +0400

    Items list

--- src/app/items/list/items-list.js
+++ src/app/items/list/items-list.js
@@ -16,4 +16,22 @@ $(function() {
             .change();
     });
 
+
+
+    $(".items-list-page .sparkline").each(function() {
+        var type = $(this).data('type');
+
+        // Generate random data
+        var data = [];
+        for (var i = 0; i < 17; i++) {
+            data.push(Math.round(100 * Math.random()));
+        }
+
+        $(this).sparkline(data, {
+            barColor: config.colorPrimary.toString(),
+            height: $(this).height(),
+            type: type
+        });
+    });
+
 });
