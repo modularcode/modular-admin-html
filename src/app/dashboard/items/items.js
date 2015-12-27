7ad8fea2824ff12d6db2f28bb43a895075b4299f
Aram Manukyan <aram.manukyan92@gmail.com>
2015-12-28 03:17:08 +0400

    Change Sparklines Collors

--- src/app/dashboard/items/items.js
+++ src/app/dashboard/items/items.js
@@ -1,31 +1,41 @@
 $(function() {
-	$(".dashboard-page .items .sparkline").each(function() {
-		var type = $(this).data('type');
+	
 
-		// There is predefined data
-		if ($(this).data('data')) {
-			var data = $(this).data('data').split(',').map(function(item) {
-				if (item.indexOf(":") > 0) {
-					return item.split(":");
-				}
-				else {
-					return item;
-				}
-			});
-		}
-		// Generate random data
-		else {
-			var data = [];
-			for (var i = 0; i < 17; i++) {
-				data.push(Math.round(100 * Math.random()));
+	function drawDashboardItemsListSparklines(){
+		$(".dashboard-page .items .sparkline").each(function() {
+			var type = $(this).data('type');
+
+			// There is predefined data
+			if ($(this).data('data')) {
+				var data = $(this).data('data').split(',').map(function(item) {
+					if (item.indexOf(":") > 0) {
+						return item.split(":");
+					}
+					else {
+						return item;
+					}
+				});
 			}
-		}
+			// Generate random data
+			else {
+				var data = [];
+				for (var i = 0; i < 17; i++) {
+					data.push(Math.round(100 * Math.random()));
+				}
+			}
 
 
-		$(this).sparkline(data, {
-			barColor: config.colorPrimary.toString(),
-			height: $(this).height(),
-			type: type
+			$(this).sparkline(data, {
+				barColor: config.colorPrimary.toString(),
+				height: $(this).height(),
+				type: type
+			});
 		});
-	});
+	}
+
+	drawDashboardItemsListSparklines();
+
+	watch(config, "colorPrimary", function(){
+        drawDashboardItemsListSparklines();
+    });
 });
