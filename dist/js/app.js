73b63a2a4e2bdfa0af14f22e74c99086efc2ef58
Gevorg Harutyunyan <gevharut@gmail.com>
2015-12-31 12:11:23 +0400

    Update deploy task

--- dist/js/app.js
+++ dist/js/app.js
@@ -314,290 +314,6 @@ function setSameHeights($container) {
 	});
 }
 
-$(function() {
-
-    if (!$('#dashboard-visits-chart').length) {
-        return false;
-    }
-
-    // drawing visits chart
-    drawVisitsChart();
-
-    var el = null;
-    var item = 'visits';
-
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-       
-       el = e.target;
-       item = $(el).attr('href').replace('#', '');
-       switchHistoryCharts(item);
-       
-    });
-
-    watch(config.chart, function(){
-        switchHistoryCharts(item);
-    });
-
-});
-
-function switchHistoryCharts(item){
-    var chartSelector = "#dashboard-" + item + "-chart";
-
-    if ($(chartSelector).has('svg').length) {
-        $(chartSelector).empty();
-    }
-
-    switch(item){
-        case 'visits':
-            drawVisitsChart();
-            break;
-         case 'downloads':
-            drawDownloadsChart();
-            break;
-    }
-}
-
-function drawVisitsChart(){
-    var dataVisits = [
-        { x: '2015-09-01', y: 70},
-        { x: '2015-09-02', y: 75 },
-        { x: '2015-09-03', y: 50},
-        { x: '2015-09-04', y: 75 },
-        { x: '2015-09-05', y: 50 },
-        { x: '2015-09-06', y: 75 },
-        { x: '2015-09-07', y: 86 } 
-    ];
-
-
-    Morris.Line({
-        element: 'dashboard-visits-chart',
-        data: dataVisits,
-        xkey: 'x',
-        ykeys: ['y'],
-        ymin: 'auto 40',
-        labels: ['Visits'],
-        xLabels: "day",
-        hideHover: 'auto',
-        yLabelFormat: function (y) {
-            // Only integers
-            if (y === parseInt(y, 10)) {
-                return y;
-            }
-            else {
-                return '';
-            }
-        },
-        resize: true,
-        lineColors: [
-            config.chart.colorSecondary.toString(),
-        ],
-        pointFillColors: [
-             config.chart.colorPrimary.toString(),
-        ]
-    });
-}
-
-function drawDownloadsChart(){
-
-    var dataDownloads = [
-        { 
-            year: '2006',
-            downloads: 1300
-        },
-        { 
-            year: '2007', 
-            downloads: 1526
-        },
-        { 
-            year: '2008', 
-            downloads: 2000
-        },
-        { 
-            year: '2009', 
-            downloads: 1800
-        },
-        { 
-            year: '2010', 
-            downloads: 1650
-        },    
-        { 
-            year: '2011', 
-            downloads: 620
-        },
-        { 
-            year: '2012', 
-            downloads: 1000
-        },
-        { 
-            year: '2013', 
-            downloads: 1896
-        },
-        { 
-            year: '2014', 
-            downloads: 850
-        },
-        { 
-            year: '2015', 
-            downloads: 1500
-        }  
-    ];
-
-
-    Morris.Bar({
-        element: 'dashboard-downloads-chart',
-        data: dataDownloads,
-        xkey: 'year',
-        ykeys: ['downloads'],
-        labels: ['Downloads'],
-        hideHover: 'auto',
-        resize: true,
-        barColors: [
-            config.chart.colorPrimary.toString(),
-            tinycolor(config.chart.colorPrimary.toString()).darken(10).toString()
-        ],
-    });
-}
-$(function() {
-	
-
-	function drawDashboardItemsListSparklines(){
-		$(".dashboard-page .items .sparkline").each(function() {
-			var type = $(this).data('type');
-
-			// There is predefined data
-			if ($(this).data('data')) {
-				var data = $(this).data('data').split(',').map(function(item) {
-					if (item.indexOf(":") > 0) {
-						return item.split(":");
-					}
-					else {
-						return item;
-					}
-				});
-			}
-			// Generate random data
-			else {
-				var data = [];
-				for (var i = 0; i < 17; i++) {
-					data.push(Math.round(100 * Math.random()));
-				}
-			}
-
-
-			$(this).sparkline(data, {
-				barColor: config.chart.colorPrimary.toString(),
-				height: $(this).height(),
-				type: type
-			});
-		});
-	}
-
-	drawDashboardItemsListSparklines();
-
-	watch(config.chart, function(){
-        drawDashboardItemsListSparklines();
-    });
-});
-$(function() {
-
-    var $dashboardSalesBreakdownChart = $('#dashboard-sales-breakdown-chart');
-
-    if (!$dashboardSalesBreakdownChart.length) {
-        return false;
-    } 
-
-    function drawSalesChart(){
-
-    $dashboardSalesBreakdownChart.empty();
-
-        Morris.Donut({
-            element: 'dashboard-sales-breakdown-chart',
-            data: [{ label: "Download Sales", value: 12 },
-                { label: "In-Store Sales", value: 30 },
-                { label: "Mail-Order Sales", value: 20 } ],
-            resize: true,
-            colors: [
-                tinycolor(config.chart.colorPrimary.toString()).lighten(10).toString(),
-                tinycolor(config.chart.colorPrimary.toString()).darken(8).toString(),
-                config.chart.colorPrimary.toString()
-            ],
-        });
-
-        var $sameheightContainer = $dashboardSalesBreakdownChart.closest(".sameheight-container");
-
-        setSameHeights($sameheightContainer);
-    }
-
-    drawSalesChart();
-
-    watch(config.chart, function(){
-       drawSalesChart();
-    });
-    
-})
-$(function() {
-
-    var $dashboardSalesMap = $('#dashboard-sales-map');
-
-    if (!$dashboardSalesMap.length) {
-        return false;
-    }
-
-    function drawSalesMap() {
-
-        $dashboardSalesMap.empty();
-
-        var color = config.chart.colorPrimary.toHexString();
-        var darkColor = tinycolor(config.chart.colorPrimary.toString()).darken(40).toHexString();
-        var selectedColor = tinycolor(config.chart.colorPrimary.toString()).darken(10).toHexString();
-
-        var sales_data = {
-            us: 2000,
-            ru: 2000, 
-            gb: 10000,
-            fr: 10000,
-            de: 10000,
-            cn: 10000,
-            in: 10000,
-            sa: 10000,
-            ca: 10000,
-            br: 5000,
-            au: 5000
-        };
-
-        $dashboardSalesMap.vectorMap({
-            map: 'world_en',
-            backgroundColor: 'transparent',
-            color: '#E5E3E5',
-            hoverOpacity: 0.7,
-            selectedColor: selectedColor,
-            enableZoom: true,
-            showTooltip: true,
-            values: sales_data,
-            scaleColors: [ color, darkColor],
-            normalizeFunction: 'linear'
-        });
-    }
-
-    drawSalesMap();
-
-    watch(config.chart, function(){
-       drawSalesMap();
-    });
-});
-$(function() {
-
-	$('.actions-list > li').on('click', '.check', function(e){
-		e.preventDefault();
-
-		$(this).parents('.tasks-item')
-		.find('.checkbox')
-		.prop("checked",  true);
-
-		removeActionList();
-	});
-
-});
 //Flot Bar Chart
 $(function() {
 
@@ -1053,19 +769,289 @@ $(function() {
         drawMorrisCharts();
     });
 });
-//LoginForm validation
 $(function() {
-	if (!$('.form-control').length) {
+
+    if (!$('#dashboard-visits-chart').length) {
         return false;
     }
 
-    $('.form-control').focus(function() {
-		$(this).siblings('.input-group-addon').addClass('focus');
+    // drawing visits chart
+    drawVisitsChart();
+
+    var el = null;
+    var item = 'visits';
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+       
+       el = e.target;
+       item = $(el).attr('href').replace('#', '');
+       switchHistoryCharts(item);
+       
+    });
+
+    watch(config.chart, function(){
+        switchHistoryCharts(item);
+    });
+
+});
+
+function switchHistoryCharts(item){
+    var chartSelector = "#dashboard-" + item + "-chart";
+
+    if ($(chartSelector).has('svg').length) {
+        $(chartSelector).empty();
+    }
+
+    switch(item){
+        case 'visits':
+            drawVisitsChart();
+            break;
+         case 'downloads':
+            drawDownloadsChart();
+            break;
+    }
+}
+
+function drawVisitsChart(){
+    var dataVisits = [
+        { x: '2015-09-01', y: 70},
+        { x: '2015-09-02', y: 75 },
+        { x: '2015-09-03', y: 50},
+        { x: '2015-09-04', y: 75 },
+        { x: '2015-09-05', y: 50 },
+        { x: '2015-09-06', y: 75 },
+        { x: '2015-09-07', y: 86 } 
+    ];
+
+
+    Morris.Line({
+        element: 'dashboard-visits-chart',
+        data: dataVisits,
+        xkey: 'x',
+        ykeys: ['y'],
+        ymin: 'auto 40',
+        labels: ['Visits'],
+        xLabels: "day",
+        hideHover: 'auto',
+        yLabelFormat: function (y) {
+            // Only integers
+            if (y === parseInt(y, 10)) {
+                return y;
+            }
+            else {
+                return '';
+            }
+        },
+        resize: true,
+        lineColors: [
+            config.chart.colorSecondary.toString(),
+        ],
+        pointFillColors: [
+             config.chart.colorPrimary.toString(),
+        ]
+    });
+}
+
+function drawDownloadsChart(){
+
+    var dataDownloads = [
+        { 
+            year: '2006',
+            downloads: 1300
+        },
+        { 
+            year: '2007', 
+            downloads: 1526
+        },
+        { 
+            year: '2008', 
+            downloads: 2000
+        },
+        { 
+            year: '2009', 
+            downloads: 1800
+        },
+        { 
+            year: '2010', 
+            downloads: 1650
+        },    
+        { 
+            year: '2011', 
+            downloads: 620
+        },
+        { 
+            year: '2012', 
+            downloads: 1000
+        },
+        { 
+            year: '2013', 
+            downloads: 1896
+        },
+        { 
+            year: '2014', 
+            downloads: 850
+        },
+        { 
+            year: '2015', 
+            downloads: 1500
+        }  
+    ];
+
+
+    Morris.Bar({
+        element: 'dashboard-downloads-chart',
+        data: dataDownloads,
+        xkey: 'year',
+        ykeys: ['downloads'],
+        labels: ['Downloads'],
+        hideHover: 'auto',
+        resize: true,
+        barColors: [
+            config.chart.colorPrimary.toString(),
+            tinycolor(config.chart.colorPrimary.toString()).darken(10).toString()
+        ],
+    });
+}
+$(function() {
+	
+
+	function drawDashboardItemsListSparklines(){
+		$(".dashboard-page .items .sparkline").each(function() {
+			var type = $(this).data('type');
+
+			// There is predefined data
+			if ($(this).data('data')) {
+				var data = $(this).data('data').split(',').map(function(item) {
+					if (item.indexOf(":") > 0) {
+						return item.split(":");
+					}
+					else {
+						return item;
+					}
+				});
+			}
+			// Generate random data
+			else {
+				var data = [];
+				for (var i = 0; i < 17; i++) {
+					data.push(Math.round(100 * Math.random()));
+				}
+			}
+
+
+			$(this).sparkline(data, {
+				barColor: config.chart.colorPrimary.toString(),
+				height: $(this).height(),
+				type: type
+			});
+		});
+	}
+
+	drawDashboardItemsListSparklines();
+
+	watch(config.chart, function(){
+        drawDashboardItemsListSparklines();
+    });
+});
+$(function() {
+
+    var $dashboardSalesBreakdownChart = $('#dashboard-sales-breakdown-chart');
+
+    if (!$dashboardSalesBreakdownChart.length) {
+        return false;
+    } 
+
+    function drawSalesChart(){
+
+    $dashboardSalesBreakdownChart.empty();
+
+        Morris.Donut({
+            element: 'dashboard-sales-breakdown-chart',
+            data: [{ label: "Download Sales", value: 12 },
+                { label: "In-Store Sales", value: 30 },
+                { label: "Mail-Order Sales", value: 20 } ],
+            resize: true,
+            colors: [
+                tinycolor(config.chart.colorPrimary.toString()).lighten(10).toString(),
+                tinycolor(config.chart.colorPrimary.toString()).darken(8).toString(),
+                config.chart.colorPrimary.toString()
+            ],
+        });
+
+        var $sameheightContainer = $dashboardSalesBreakdownChart.closest(".sameheight-container");
+
+        setSameHeights($sameheightContainer);
+    }
+
+    drawSalesChart();
+
+    watch(config.chart, function(){
+       drawSalesChart();
+    });
+    
+})
+$(function() {
+
+    var $dashboardSalesMap = $('#dashboard-sales-map');
+
+    if (!$dashboardSalesMap.length) {
+        return false;
+    }
+
+    function drawSalesMap() {
+
+        $dashboardSalesMap.empty();
+
+        var color = config.chart.colorPrimary.toHexString();
+        var darkColor = tinycolor(config.chart.colorPrimary.toString()).darken(40).toHexString();
+        var selectedColor = tinycolor(config.chart.colorPrimary.toString()).darken(10).toHexString();
+
+        var sales_data = {
+            us: 2000,
+            ru: 2000, 
+            gb: 10000,
+            fr: 10000,
+            de: 10000,
+            cn: 10000,
+            in: 10000,
+            sa: 10000,
+            ca: 10000,
+            br: 5000,
+            au: 5000
+        };
+
+        $dashboardSalesMap.vectorMap({
+            map: 'world_en',
+            backgroundColor: 'transparent',
+            color: '#E5E3E5',
+            hoverOpacity: 0.7,
+            selectedColor: selectedColor,
+            enableZoom: true,
+            showTooltip: true,
+            values: sales_data,
+            scaleColors: [ color, darkColor],
+            normalizeFunction: 'linear'
+        });
+    }
+
+    drawSalesMap();
+
+    watch(config.chart, function(){
+       drawSalesMap();
+    });
+});
+$(function() {
+
+	$('.actions-list > li').on('click', '.check', function(e){
+		e.preventDefault();
+
+		$(this).parents('.tasks-item')
+		.find('.checkbox')
+		.prop("checked",  true);
+
+		removeActionList();
 	});
 
-	$('.form-control').blur(function() {
-		$(this).siblings('.input-group-addon').removeClass('focus');
-	});
 });
 //LoginForm validation
 $(function() {
