b888127d2faefd2e0a8ff8465c5d2f0002686544
Gevorg Harutyunyan <gevharut@gmail.com>
2016-01-07 05:34:33 +0400

    Enable layouts inheritance

--- build/tasks/app-pages.js
+++ build/tasks/app-pages.js
@@ -2,61 +2,44 @@ var path 	= require('path');
 var glob 	= require('glob');
 var fs 		= require('fs-extra');
 var through = require('through2');
+var File = require('vinyl');
+var StringDecoder = require('string_decoder').StringDecoder;
 var extend 	= require('util')._extend;
 
+var frontMatter = require('front-matter');
+var handlebars = require('handlebars');
+var handlebarsRegistrar = require('handlebars-registrar');
+
 var config 	= require('../config');
+var partials = {};
 
 module.exports.task = function(gulp, plugins, paths) {
 
-	// Handlebars engine
-	var handlebars = new require('handlebars');
-	var handlebarsRegistrar = require('handlebars-registrar');
 
 	// Register handlebars engine helpers and partials
 	handlebarsRegistrar(handlebars, {
 		helpers: paths.app.helpers,
 		partials: paths.app.templates,
-		parsePartialName: function (file) {
-			return file.shortPath;
+		parsePartialName: function (partial) {
+
+			// Save in partials vinyl registry
+			partials[partial.shortPath] = new File({
+				cwd: partial.cwd,
+				path: partial.path,
+				base: path.basename(partial.path),
+				contents: fs.readFileSync(partial.path)
+			});
+
+			return partial.shortPath;
 		},
+		bustCache: true,
 	});
 
+
 	gulp.src(paths.app.pages)
-		// Frontmatter
-		.pipe(plugins.frontMatter())
 		// Render pages
 		.pipe(through.obj(function (file, enc, cb) {
-			// Page render result
-			var pageRes = "";
-
-			// Get context from _context.js files and frontmatter
-			var context = getPageContext(file);
-				context = extend(context, file.frontMatter);
-
-			// Compile template
-			var template = handlebars.compile(String(file.contents));
-			var templateRes = template(context);
-
-			// Layout processing
-			var layout = context.layout || null;
-
-			// If the layout exists, render it with template inside
-			if (layout && handlebars.partials[layout]) {
-				var layoutData = extend(context, {
-					body: templateRes
-				});
-
-				// Render layout with given context and content
-				var layoutRes = handlebars.partials[layout](layoutData);
-
-				pageRes = layoutRes;
-			}
-			// Return rendered template
-			else {
-				pageRes = templateRes;
-			}
-
-			file.contents = new Buffer(pageRes);
+			file.contents = new Buffer(renderTemplate(file));
 
 			this.push(file);
 			cb();
@@ -75,12 +58,90 @@ module.exports.task = function(gulp, plugins, paths) {
 
 		// Output
 		.pipe(gulp.dest(config.destDir));
+	
 };
 
 
 /********************************************
 *				Utils
 *********************************************/
+
+function renderTemplate(file, options) {
+
+	options = options || {};
+
+	// Set file frontMatter
+	file = setFrontMatter(file);
+
+	// Get context from _context.js files and frontmatter
+	var contextExternal = getPageContextExternal(file);
+
+	// Frontmatter context
+	var contextTemplate = file.frontMatter || {};
+
+	// Inherited context from child
+	var contextInherited = options.contextInherited || {};
+
+	// Result context
+	var	context = extend({}, 	  contextExternal);
+		context = extend(context, contextTemplate);
+		context = extend(context, contextInherited);
+
+	// Page render result
+	var pageRes = "";
+
+	// Compile template
+	var template = handlebars.compile(String(file.contents));
+	var templateRes = template(context);
+
+	// Layout processing
+	var layout = context.layout || null;
+
+	// If the layout exists, render it with template inside
+	if (layout && partials[layout] && handlebars.partials[layout]) {
+
+		// New instance of context
+		var layoutData = extend({}, context);
+
+		// Add body to context
+		layoutData = extend(layoutData, {
+			body: templateRes
+		});
+
+		// Remove layout parameter from inhereted context
+		delete layoutData.layout;
+
+		// New vinyl file based on partail vinyl
+		var layoutFile = new File(partials[layout]);
+
+		// Call recursively render template again
+		pageRes = renderTemplate(layoutFile, {
+			contextInherited: layoutData
+		});
+	}
+	// Return rendered template
+	else {
+		pageRes = templateRes;
+	}
+
+	return pageRes;
+}
+
+
+/*
+	Frontmatter file
+*/
+function setFrontMatter(file) {
+	// Read content from front matter
+	var content = frontMatter(file.contents.toString('utf8'));
+
+	// var res = new Buffer(content.body);
+	file.contents = new Buffer(content.body);
+	file.frontMatter = content.attributes;
+
+	return file;
+}
+
 
 /*
 	This function returns context of current page 
@@ -89,7 +150,7 @@ module.exports.task = function(gulp, plugins, paths) {
 */
 
 
-function getPageContext(file) {
+function getPageContextExternal(file) {
 
 	var context = {};
 
