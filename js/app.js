6f076142d5503dea0934c34a508dc23e08715e0b
Gevorg Harutyunyan <gevharut@gmail.com>
2017-06-22 04:12:40 +0400

    Update 2017-06-22T00:12:24.335Z

--- js/app.js
+++ js/app.js
@@ -49,178 +49,6 @@ config.chart = {};
 
 config.chart.colorPrimary = tinycolor($ref.find(".chart .color-primary").css("color"));
 config.chart.colorSecondary = tinycolor($ref.find(".chart .color-secondary").css("color"));
-$(function() {
-	animate({
-		name: 'flipInY',
-		selector: '.error-card > .error-title-block'
-	});
-
-
-	setTimeout(function(){
-		var $el = $('.error-card > .error-container');
-
-		animate({
-			name: 'fadeInUp',
-			selector: $el 
-		});
-
-		$el.addClass('visible');
-	}, 1000);
-})
-//LoginForm validation
-$(function() {
-	if (!$('#login-form').length) {
-        return false;
-    }
-
-    var loginValidationSettings = {
-	    rules: {
-	        username: {
-	            required: true,
-	            email: true
-	        },
-	        password: "required",
-	        agree: "required"
-	    },
-	    messages: {
-	        username: {
-	            required: "Please enter username",
-	            email: "Please enter a valid email address"
-	        },
-	        password:  "Please enter password",
-	        agree: "Please accept our policy"
-	    },
-	    invalidHandler: function() {
-			animate({
-				name: 'shake',
-				selector: '.auth-container > .card'
-			});
-		}
-	}
-
-	$.extend(loginValidationSettings, config.validations);
-
-    $('#login-form').validate(loginValidationSettings);
-})
-//ResetForm validation
-$(function() {
-	if (!$('#reset-form').length) {
-        return false;
-    }
-
-    var resetValidationSettings = {
-	    rules: {
-	        email1: {
-	            required: true,
-	            email: true
-	        }
-	    },
-	    messages: {
-	        email1: {
-	            required: "Please enter email address",
-	            email: "Please enter a valid email address"
-	        }
-	    },
-	    invalidHandler: function() {
-			animate({
-				name: 'shake',
-				selector: '.auth-container > .card'
-			});
-		}
-	}
-
-	$.extend(resetValidationSettings, config.validations);
-
-    $('#reset-form').validate(resetValidationSettings);
-})
-//SignupForm validation
-$(function() {
-	if (!$('#signup-form').length) {
-        return false;
-    }
-
-    var signupValidationSettings = {
-	    rules: {
-	    	firstname: {
-	    		required: true,
-	    	},
-	    	lastname: {
-	    		required: true,
-	    	},
-	        email: {
-	            required: true,
-	            email: true
-	        },
-	        password: {
-				required: true,
-				minlength: 8
-	        },
-	        retype_password: {
-				required: true,
-				minlength: 8,
-				equalTo: "#password"
-			},
-			agree: {
-				required: true,
-			}
-	    },
-	    groups: {
-	    	name: "firstname lastname",
-			pass: "password retype_password",
-		},
-		errorPlacement: function(error, element) {
-			if (
-				element.attr("name") == "firstname" || 
-				element.attr("name") == "lastname" 
-			) {
-				error.insertAfter($("#lastname").closest('.row'));
-				element.parents("div.form-group")
-				.addClass('has-error');
-			} 
-			else if (
-				element.attr("name") == "password" || 
-				element.attr("name") == "retype_password" 
-			) {
-				error.insertAfter($("#retype_password").closest('.row'));
-				element.parents("div.form-group")
-				.addClass('has-error');
-			}
-			else if (element.attr("name") == "agree") {
-				error.insertAfter("#agree-text");
-			}
-			else {
-				error.insertAfter(element);
-			}
-		},
-	    messages: {
-	    	firstname: "Please enter firstname and lastname",
-	    	lastname: "Please enter firstname and lastname",
-	        email: {
-	            required: "Please enter email",
-	            email: "Please enter a valid email address"
-	        },
-	        password: {
-	        	required: "Please enter password fields.",
-	        	minlength: "Passwords should be at least 8 characters."
-	        },
-	        retype_password: {
-	        	required: "Please enter password fields.",
-	        	minlength: "Passwords should be at least 8 characters."
-	        },
-	        agree: "Please accept our policy"
-	    },
-	    invalidHandler: function() {
-			animate({
-				name: 'shake',
-				selector: '.auth-container > .card'
-			});
-		}
-	}
-
-	$.extend(signupValidationSettings, config.validations);
-
-    $('#signup-form').validate(signupValidationSettings);
-});
 /***********************************************
 *        Animation Settings
 ***********************************************/
@@ -310,6 +138,217 @@ function setSameHeights($container) {
 	});
 }
 
+$(function() {
+	animate({
+		name: 'flipInY',
+		selector: '.error-card > .error-title-block'
+	});
+
+
+	setTimeout(function(){
+		var $el = $('.error-card > .error-container');
+
+		animate({
+			name: 'fadeInUp',
+			selector: $el 
+		});
+
+		$el.addClass('visible');
+	}, 1000);
+})
+//LoginForm validation
+$(function() {
+	if (!$('#login-form').length) {
+        return false;
+    }
+
+    var loginValidationSettings = {
+	    rules: {
+	        username: {
+	            required: true,
+	            email: true
+	        },
+	        password: "required",
+	        agree: "required"
+	    },
+	    messages: {
+	        username: {
+	            required: "Please enter username",
+	            email: "Please enter a valid email address"
+	        },
+	        password:  "Please enter password",
+	        agree: "Please accept our policy"
+	    },
+	    invalidHandler: function() {
+			animate({
+				name: 'shake',
+				selector: '.auth-container > .card'
+			});
+		}
+	}
+
+	$.extend(loginValidationSettings, config.validations);
+
+    $('#login-form').validate(loginValidationSettings);
+})
+//SignupForm validation
+$(function() {
+	if (!$('#signup-form').length) {
+        return false;
+    }
+
+    var signupValidationSettings = {
+	    rules: {
+	    	firstname: {
+	    		required: true,
+	    	},
+	    	lastname: {
+	    		required: true,
+	    	},
+	        email: {
+	            required: true,
+	            email: true
+	        },
+	        password: {
+				required: true,
+				minlength: 8
+	        },
+	        retype_password: {
+				required: true,
+				minlength: 8,
+				equalTo: "#password"
+			},
+			agree: {
+				required: true,
+			}
+	    },
+	    groups: {
+	    	name: "firstname lastname",
+			pass: "password retype_password",
+		},
+		errorPlacement: function(error, element) {
+			if (
+				element.attr("name") == "firstname" || 
+				element.attr("name") == "lastname" 
+			) {
+				error.insertAfter($("#lastname").closest('.row'));
+				element.parents("div.form-group")
+				.addClass('has-error');
+			} 
+			else if (
+				element.attr("name") == "password" || 
+				element.attr("name") == "retype_password" 
+			) {
+				error.insertAfter($("#retype_password").closest('.row'));
+				element.parents("div.form-group")
+				.addClass('has-error');
+			}
+			else if (element.attr("name") == "agree") {
+				error.insertAfter("#agree-text");
+			}
+			else {
+				error.insertAfter(element);
+			}
+		},
+	    messages: {
+	    	firstname: "Please enter firstname and lastname",
+	    	lastname: "Please enter firstname and lastname",
+	        email: {
+	            required: "Please enter email",
+	            email: "Please enter a valid email address"
+	        },
+	        password: {
+	        	required: "Please enter password fields.",
+	        	minlength: "Passwords should be at least 8 characters."
+	        },
+	        retype_password: {
+	        	required: "Please enter password fields.",
+	        	minlength: "Passwords should be at least 8 characters."
+	        },
+	        agree: "Please accept our policy"
+	    },
+	    invalidHandler: function() {
+			animate({
+				name: 'shake',
+				selector: '.auth-container > .card'
+			});
+		}
+	}
+
+	$.extend(signupValidationSettings, config.validations);
+
+    $('#signup-form').validate(signupValidationSettings);
+});
+//ResetForm validation
+$(function() {
+	if (!$('#reset-form').length) {
+        return false;
+    }
+
+    var resetValidationSettings = {
+	    rules: {
+	        email1: {
+	            required: true,
+	            email: true
+	        }
+	    },
+	    messages: {
+	        email1: {
+	            required: "Please enter email address",
+	            email: "Please enter a valid email address"
+	        }
+	    },
+	    invalidHandler: function() {
+			animate({
+				name: 'shake',
+				selector: '.auth-container > .card'
+			});
+		}
+	}
+
+	$.extend(resetValidationSettings, config.validations);
+
+    $('#reset-form').validate(resetValidationSettings);
+})
+$(function() {
+
+	$(".wyswyg").each(function() {
+
+		var $toolbar = $(this).find(".toolbar");
+		var $editor = $(this).find(".editor");
+
+
+		var editor = new Quill($editor.get(0), {
+			theme: 'snow'
+		});
+
+		editor.addModule('toolbar', {
+			container: $toolbar.get(0)     // Selector for toolbar container
+		});
+
+
+
+	});
+	
+});
+$(function () {
+
+	$('#sidebar-menu, #customize-menu').metisMenu({
+		activeClass: 'open'
+	});
+
+
+	$('#sidebar-collapse-btn').on('click', function(event){
+		event.preventDefault();
+		
+		$("#app").toggleClass("sidebar-open");
+	});
+
+	$("#sidebar-overlay").on('click', function() {
+		$("#app").removeClass("sidebar-open");
+	});
+	
+});
 //Flot Bar Chart
 $(function() {
 
@@ -1052,40 +1091,20 @@ $(function() {
 	});
 
 });
-$(function(){
+//LoginForm validation
+$(function() {
+	if (!$('.form-control').length) {
+        return false;
+    }
 
-	// set sortable options
-	$('.images-container').sortable({
-		animation: 150,
-		handle: ".control-btn.move",
-		draggable: ".image-container",
-		onMove: function (evt) {
-			var $relatedElem = $(evt.related);
-
-	        if ($relatedElem.hasClass('add-image')) {
-	        	return false;
-	        }
-	    }
+    $('.form-control').focus(function() {
+		$(this).siblings('.input-group-addon').addClass('focus');
 	});
 
-
-	$controlsButtons = $('.controls');
-
-	$controlsButtonsStar = $controlsButtons.find('.star');
-	$controlsButtonsRemove = $controlsButtons.find('.remove');
-
-	$controlsButtonsStar.on('click',function(e){
-		e.preventDefault();
-
-		$controlsButtonsStar.removeClass('active');
-		$controlsButtonsStar.parents('.image-container').removeClass('main');
-
-		$(this).addClass('active');
-
-		$(this).parents('.image-container').addClass('main');
-	})
-
-})
+	$('.form-control').blur(function() {
+		$(this).siblings('.input-group-addon').removeClass('focus');
+	});
+});
 $(function() {
 
     if (!$('#select-all-items').length) {
@@ -1130,92 +1149,40 @@ $(function() {
     });
 
 });
-//LoginForm validation
-$(function() {
-	if (!$('.form-control').length) {
-        return false;
-    }
+$(function(){
 
-    $('.form-control').focus(function() {
-		$(this).siblings('.input-group-addon').addClass('focus');
-	});
+	// set sortable options
+	$('.images-container').sortable({
+		animation: 150,
+		handle: ".control-btn.move",
+		draggable: ".image-container",
+		onMove: function (evt) {
+			var $relatedElem = $(evt.related);
 
-	$('.form-control').blur(function() {
-		$(this).siblings('.input-group-addon').removeClass('focus');
-	});
-});
-$(function() {
-
-	$(".wyswyg").each(function() {
-
-		var $toolbar = $(this).find(".toolbar");
-		var $editor = $(this).find(".editor");
-
-
-		var editor = new Quill($editor.get(0), {
-			theme: 'snow'
-		});
-
-		editor.addModule('toolbar', {
-			container: $toolbar.get(0)     // Selector for toolbar container
-		});
-
-
-
-	});
-	
-});
-$(function () {
-
-	$('#sidebar-menu, #customize-menu').metisMenu({
-		activeClass: 'open'
+	        if ($relatedElem.hasClass('add-image')) {
+	        	return false;
+	        }
+	    }
 	});
 
 
-	$('#sidebar-collapse-btn').on('click', function(event){
-		event.preventDefault();
-		
-		$("#app").toggleClass("sidebar-open");
-	});
+	$controlsButtons = $('.controls');
 
-	$("#sidebar-overlay").on('click', function() {
-		$("#app").removeClass("sidebar-open");
-	});
-	
-});
-$(function() {
-	$('.nav-profile > li > a').on('click', function() {
-		var $el = $(this).next();
+	$controlsButtonsStar = $controlsButtons.find('.star');
+	$controlsButtonsRemove = $controlsButtons.find('.remove');
 
-		animate({
-			name: 'flipInX',
-			selector: $el
-		});
-	});
+	$controlsButtonsStar.on('click',function(e){
+		e.preventDefault();
+
+		$controlsButtonsStar.removeClass('active');
+		$controlsButtonsStar.parents('.image-container').removeClass('main');
+
+		$(this).addClass('active');
+
+		$(this).parents('.image-container').addClass('main');
+	})
+
 })
-var modalMedia = {
-	$el: $("#modal-media"),
-	result: {},
-	options: {},
-	open: function(options) {
-		options = options || {};
-		this.options = options;
-
-
-		this.$el.modal('show');
-	},
-	close: function() {
-		if ($.isFunction(this.options.beforeClose)) {
-			this.options.beforeClose(this.result);
-		}
-
-		this.$el.modal('hide');
-
-		if ($.isFunction(this.options.afterClose)) {
-			this.options.beforeClose(this.result);
-		}
-	}
-};
 $(function () {
 
 	// Local storage settings
@@ -1371,6 +1338,39 @@ $(function () {
 	}
 
 });
+$(function() {
+	$('.nav-profile > li > a').on('click', function() {
+		var $el = $(this).next();
+
+		animate({
+			name: 'flipInX',
+			selector: $el
+		});
+	});
+})
+var modalMedia = {
+	$el: $("#modal-media"),
+	result: {},
+	options: {},
+	open: function(options) {
+		options = options || {};
+		this.options = options;
+
+
+		this.$el.modal('show');
+	},
+	close: function() {
+		if ($.isFunction(this.options.beforeClose)) {
+			this.options.beforeClose(this.result);
+		}
+
+		this.$el.modal('hide');
+
+		if ($.isFunction(this.options.afterClose)) {
+			this.options.beforeClose(this.result);
+		}
+	}
+};
 $(function() {
 
 	$("body").addClass("loaded");
